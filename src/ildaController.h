58bb6bd3f83796d82a1d613561b34cdeff688f09
Eduard Frigola <eduardfrigola@gmail.com>
2025-01-23 12:54:04 +0100

    Create default beamZone for default laser

--- src/ildaController.h
+++ src/ildaController.h
@@ -20,6 +20,8 @@ public:
 //		laser.globalBrightness = 1;
 //        laser.resetAllLasersToDefault();
 //        zone = laser.addCanvasZone(0, 0, -1, -1);
+        ofxLaser::ZoneId zoneId2 = laser.createNewBeamZone();
+        laser.addZoneToLaser(zoneId2, 0);
     };
     ~ildaController(){};
     
